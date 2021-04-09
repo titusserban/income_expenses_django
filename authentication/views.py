991a77b8ced0960094bd99f406972c65d3e8fe0c
unknown <titus.brasov@gmail.com>
2021-04-09 15:35:09 +0300

    WIP

---
+++ authentication/views.py
@@ -0,0 +1,86 @@
+  
+from django.shortcuts import render, redirect
+from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from validate_email import validate_email
+from django.contrib import messages
+from django.core.mail import EmailMessage
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.encoding import force_bytes, force_text, DjangoUnicodeDecodeError
+from django.core.mail import send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.template.loader import render_to_string
+# from .utils import account_activation_token
+from django.urls import reverse
+from django.contrib import auth
+
+# Create your views here.
+
+class EmailValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        email = data['email']
+
+        if not validate_email(email):
+            return JsonResponse({'email_error': 'The email is invalid'}, status = 400)
+        if User.objects.filter(email=email).exists():
+            return JsonResponse({'email_error': 'The email you chose is already in use. Please choose another one.'}, status = 409)    
+        return JsonResponse({'email_valid': True})
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'}, status = 400)
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'The username you chose is already in use. Please choose another one.'}, status = 409)    
+        return JsonResponse({'username_valid': True})
+
+class RegistrationView(View):
+    def get(self, request):
+        return render(request, 'authentication/register.html')
+
+    def post(self, request):
+        # GET USER DATA
+        # VALIDATE
+        # create a user account
+
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+
+        context = {
+            'fieldValues': request.POST
+        }
+        if not User.objects.filter(username=username).exists():
+            if not User.objects.filter(email=email).exists():
+                if len(password) < 6:
+                    messages.error(request, 'The password is too short.')
+                    return render(request, 'authentication/register.html', context)
+
+                user = User.objects.create_user(username=username, email=email)
+                user.set_password(password)
+                user.is_active = False
+                user.save()
+                email_body = ""
+                email_subject = 'Activate your account'
+                email = EmailMessage(
+                    email_subject,
+                    'Hi '+user.username + ', Please the link below to activate your account \n',
+                    'noreply@semycolon.com',
+                    [email],
+                )
+                email.send(fail_silently=False)
+                messages.success(request, 'Account successfully created')
+                return render(request, 'authentication/register.html')
+
+        return render(request, 'authentication/register.html')
