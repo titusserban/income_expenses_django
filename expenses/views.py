0c2d73a43c64a1ca4af9bb467a0b93e1b3513f5e
unknown <titus.brasov@gmail.com>
2021-04-17 22:14:35 +0300

    CRUD for income - DONE

--- expenses/views.py
+++ expenses/views.py
@@ -4,8 +4,21 @@ from .models import Category, Expense
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
+import json
+from django.http import JsonResponse
+from userpreferences.models import UserPreference
+import datetime
 
 # Create your views here.
+
+
+def search_expenses(request):
+    if request.method == 'POST':
+        search_str = json.loads(request.body).get('searchText')
+        expenses = Expense.objects.filter(amount__istartswith=search_str, owner=request.user) | Expense.objects.filter(date__istartswith=search_str, owner=request.user) | Expense.objects.filter(description__icontains=search_str, owner=request.user) | Expense.objects.filter(category__icontains=search_str, owner=request.user)
+
+        data = expenses.values()   
+        return JsonResponse(list(data), safe=False)
 
 @login_required(login_url='/authentication/login')
 def index(request):
@@ -14,9 +27,11 @@ def index(request):
     paginator = Paginator(expenses, 2)
     page_number = request.GET.get('page')
     page_obj = Paginator.get_page(paginator, page_number)
+    currency = UserPreference.objects.get(user=request.user).currency
     context = {
         'expenses': expenses,
         'page_obj': page_obj,
+        'currency': currency,
     }
     return render(request, 'expenses/index.html', context)
 
